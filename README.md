e75c0de4b3ada41543169d4cf1116a7c1ae5f17c
jasper <jasper.baes@gmail.com>
2020-09-29 21:42:19 +0200

    add changes

---
+++ README.md
@@ -0,0 +1,8 @@
+# Curl project
+
+Animated frames displayed by `curl`.
+
+# Collaborators
+
+- [Jasper Baes](https://jbaes.be)
+- Sandro Deblonde
---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('mz/fs');
+const http = require('http');
+const {Readable} = require('stream');
+const colors = require('colors/safe');
+
+const frames = [];
+
+// Setup frames in memory
+fs.readdir('./frames').then(data => { 
+  data.forEach(async frame => {
+    const f = await fs.readFile(`./frames/${frame}`);
+    frames.push(f.toString());
+  })
+});
+
+const colorsOptions = ['red', 'yellow', 'green', 'blue', 'magenta', 'cyan', 'white'];
+
+const streamer = stream => {
+  let index = 0;
+  return setInterval(() => {
+    if (index > frames.length) index = 0; stream.push('\033c');
+    const c = colorsOptions[Math.floor(Math.random() * colorsOptions.length)];
+    stream.push(colors[c](frames[index]));
+    index++;
+  }, 120);
+}
+
+const server = http.createServer((req, res) => {
+  if (!req.headers['user-agent'].includes('curl')) {
+    res.writeHead(302, {'Location': 'https://twitter.com/baesjasper'});
+    return res.end();
+  }
+  const stream = new Readable();
+  stream._read = function noop () {};
+  stream.pipe(res);
+  const interval = streamer(stream);
+
+  req.on('close', () => {
+    stream.destroy();
+    clearInterval(interval);
+  });
+});
+
+
+const port = process.env.PARROT_PORT || 3000;
+server.listen(port, '0.0.0.0', err => {
+  if (err) throw err;
+  console.log(`Listening on locahost:${port}`);
+});
